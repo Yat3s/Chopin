49c269766554da3c0ea6190735fa3db4aa8868fd
Yat3s <4645643@qq.com>
2017-05-24 19:13:54 +0800

    Added decoration test case

---
+++ app/src/main/java/com/yat3s/nimblerecyclerview/HeaderItemDecoration.java
@@ -0,0 +1,38 @@
+package com.yat3s.nimblerecyclerview;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Yat3s on 24/05/2017.
+ * Email: hawkoyates@gmail.com
+ * GitHub: https://github.com/yat3s
+ */
+public class HeaderItemDecoration extends RecyclerView.ItemDecoration {
+    private Paint mPaint;
+
+    public HeaderItemDecoration() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+        c.drawRect(0, 0, parent.getWidth(), parent.getChildAt(0).getHeight(), mPaint);
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+}
--- app/src/main/java/com/yat3s/nimblerecyclerview/MainActivity.java
+++ app/src/main/java/com/yat3s/nimblerecyclerview/MainActivity.java
@@ -1,18 +1,31 @@
 package com.yat3s.nimblerecyclerview;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.yat3s.nimblerecyclerview.util.BaseAdapter;
+import com.yat3s.nimblerecyclerview.util.BaseViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,5 +37,44 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mRecyclerView.addItemDecoration(new HeaderItemDecoration());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new TodoAdapter(this, generateMockData()));
+    }
+
+    private List<Task> generateMockData() {
+        List<Task> tasks = new ArrayList<>();
+        String[] titles = {"Abby", "Romy", "Aran", "Abby", "Romy", "Aran", "Abby", "Romy", "Aran"};
+        for (String title : titles) {
+            tasks.add(new Task(title));
+        }
+
+        return tasks;
+    }
+
+    static class TodoAdapter extends BaseAdapter<Task> {
+        public TodoAdapter(Context context, List<Task> data) {
+            super(context, data);
+        }
+
+        @Override
+        protected void bindDataToItemView(BaseViewHolder holder, Task data, int position) {
+            holder.setText(R.id.title_tv, data.title);
+            holder.itemView.setBackgroundColor((position & 1) == 0 ? Color.parseColor("#DDDDDD") : Color.WHITE);
+        }
+
+        @Override
+        protected int getItemViewLayoutId(int position, Task data) {
+            return R.layout.item_task;
+        }
+    }
+
+    static class Task {
+        public String title;
+
+        public Task(String title) {
+            this.title = title;
+        }
     }
 }
