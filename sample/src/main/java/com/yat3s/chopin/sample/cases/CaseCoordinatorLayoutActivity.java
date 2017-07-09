372603e3c9bbeb4c60802185a7ea2b4a1d6a477f
Yat3s <4645643@qq.com>
2017-07-09 21:22:12 +0800

    Added coordinator layout refresh demo

--- sample/src/main/java/com/yat3s/chopin/sample/cases/CaseCoordinatorLayoutActivity.java
+++ sample/src/main/java/com/yat3s/chopin/sample/cases/CaseCoordinatorLayoutActivity.java
@@ -1,5 +1,11 @@
 package com.yat3s.chopin.sample.cases;
 
+import android.support.design.widget.AppBarLayout;
+import android.util.Log;
+import android.view.View;
+
+import com.yat3s.chopin.ChopinLayout;
+import com.yat3s.chopin.ViewScrollChecker;
 import com.yat3s.chopin.sample.R;
 
 /**
@@ -8,6 +14,10 @@ import com.yat3s.chopin.sample.R;
  * GitHub: https://github.com/yat3s
  */
 public class CaseCoordinatorLayoutActivity extends BaseCaseActivity {
+    private static final String TAG = "CaseCoordinatorLayoutAc";
+
+    private int mCurrentVerticalOffset = -1, mTotalScrollRange;
+
     @Override
     protected int getContentLayoutId() {
         return R.layout.case_activity_coordinator;
@@ -16,6 +26,29 @@ public class CaseCoordinatorLayoutActivity extends BaseCaseActivity {
     @Override
     protected void initialize() {
         setupRefreshHeader("refresh.json", 0.2f, 3000);
-        setupLoadingFooter("Plane.json", 0.2f, 1500);
+//        setupLoadingFooter("Plane.json", 0.2f, 1500);
+
+        final AppBarLayout barLayout = (AppBarLayout) findViewById(R.id.flexible_example_appbar);
+        barLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                mCurrentVerticalOffset = verticalOffset;
+                Log.d(TAG, "onOffsetChanged: " + verticalOffset);
+                Log.d(TAG, "getTotalScrollRange: " + barLayout.getTotalScrollRange());
+            }
+        });
+
+        mChopinLayout.setViewScrollChecker(new ViewScrollChecker() {
+            @Override
+            public boolean canDoRefresh(ChopinLayout chopinLayout, View contentView) {
+                return mCurrentVerticalOffset == 0;
+            }
+
+            @Override
+            public boolean canDoLoading(ChopinLayout chopinLayout, View contentView) {
+                return false;
+            }
+        });
+
     }
 }
