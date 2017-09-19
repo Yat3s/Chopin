e657567a24538183df2a20ea8374d71f6c0f93ba
Yat3s <4645643@qq.com>
2017-09-19 18:03:16 +0800

    Added music recycler view case

--- sample/src/main/java/com/yat3s/chopin/sample/DataRepository.java
+++ sample/src/main/java/com/yat3s/chopin/sample/DataRepository.java
@@ -18,8 +18,8 @@ public class DataRepository {
     public static ArrayList<MusicAdapter.Music> generateMusicData() {
         final String[] musicNames = ChopinApplication.getContext().getResources().getStringArray(musics);
         ArrayList<MusicAdapter.Music> musics = new ArrayList<>();
-        for (String taskName : musicNames) {
-            musics.add(new MusicAdapter.Music(taskName, generateRandomImageResId()));
+        for (String musicName : musicNames) {
+            musics.add(new MusicAdapter.Music(musicName, generateRandomImageResId()));
         }
         return musics;
     }
@@ -28,4 +28,14 @@ public class DataRepository {
         int randomIndex = (int) (Math.random() * ABSTRACT_IMAGE_RESOURCE_ID.length);
         return ABSTRACT_IMAGE_RESOURCE_ID[randomIndex];
     }
+
+//    private static List<PostcardAdapter.Postcard> generatePostcardData(int size) {
+//        int[] postcardImageIds = {R.mipmap.card_1, R.mipmap.card_2, R.mipmap.card_3,
+//                R.mipmap.card_4, R.mipmap.card_5, R.mipmap.card_6, R.mipmap.card_7};
+//        List<PostcardAdapter.Postcard> postcards = new ArrayList<>();
+//        for (int idx = 0; idx < size; idx++) {
+//            postcards.add(new PostcardAdapter.Postcard(postcardImageIds[idx % postcardImageIds.length]));
+//        }
+//        return postcards;
+//    }
 }
--- sample/src/main/java/com/yat3s/chopin/sample/cases/CaseNestedRecyclerViewActivity.java
+++ sample/src/main/java/com/yat3s/chopin/sample/cases/CaseNestedRecyclerViewActivity.java
@@ -3,11 +3,9 @@ package com.yat3s.chopin.sample.cases;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.yat3s.chopin.sample.PostcardAdapter;
+import com.yat3s.chopin.sample.DataRepository;
+import com.yat3s.chopin.sample.MusicAdapter;
 import com.yat3s.chopin.sample.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Yat3s on 2017/7/6 0006.
@@ -15,8 +13,6 @@ import java.util.List;
  * GitHub: https://github.com/yat3s
  */
 public class CaseNestedRecyclerViewActivity extends BaseCaseActivity {
-    private static final int MOCK_ITEM_SIZE = 12;
-
     @Override
     protected int getContentLayoutId() {
         return R.layout.case_activity_nested_recycler_view;
@@ -29,18 +25,8 @@ public class CaseNestedRecyclerViewActivity extends BaseCaseActivity {
 
         // Configure adapter.
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        PostcardAdapter postcardAdapter = new PostcardAdapter(this, generatePostcardData(MOCK_ITEM_SIZE));
+        MusicAdapter musicAdapter = new MusicAdapter(this, DataRepository.generateMusicData());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(postcardAdapter);
-    }
-
-    private List<PostcardAdapter.Postcard> generatePostcardData(int size) {
-        int[] postcardImageIds = {R.mipmap.card_1, R.mipmap.card_2, R.mipmap.card_3,
-                R.mipmap.card_4, R.mipmap.card_5, R.mipmap.card_6, R.mipmap.card_7};
-        List<PostcardAdapter.Postcard> postcards = new ArrayList<>();
-        for (int idx = 0; idx < size; idx++) {
-            postcards.add(new PostcardAdapter.Postcard(postcardImageIds[idx % postcardImageIds.length]));
-        }
-        return postcards;
+        recyclerView.setAdapter(musicAdapter);
     }
 }
